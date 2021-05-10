592035d89bbd08cc06f6845d2a239fffe88ff295
diot24 <34337000+diot24@users.noreply.github.com>
2021-05-10 13:01:50 +0530

    Update edge.py

--- edge.py
+++ edge.py
@@ -1,33 +1,16 @@
 # You have to write a forever executable python program to read sensor data and send it to the cloud server.
+import socket
+import pandas
 
-import serial
-import time
-import csv
+df = pandas.read_csv('dataset.csv')
+print(df)
 
-# set up the serial line
-ser = serial.Serial('COM4', 9600)
-time.sleep(2)
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 80        # The port used by the server
 
-# Read and record the data
-data =[]                       # empty list to store the data
-for i in range(50):
-	b = ser.readline()         # read a byte string
-        string_n = b.decode()  # decode byte string into Unicode  
-	string = string_n.rstrip() # remove \n and \r
-	print(string)
-	data.append(string)           # add to the end of data list
-	time.sleep(60)            # wait (sleep) 0.1 seconds
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(df)
+    data = s.recv(1024)
 
-ser.close()
-
-# Open and save data to csv file
-with open('sensor_data.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Timestamp", "Value", "Sensor"])
-    timestamp = message['timestamp']
-    value   = message['vaue']
-    sensor  = message['sensor']
-    with open('results.csv', 'a+') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow([timestamp, value, sensor])
-    
+print('Received', repr(data))
