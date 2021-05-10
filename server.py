ac9451e16a14c254f21d326ced37d38b502ce23f
diot24 <34337000+diot24@users.noreply.github.com>
2021-05-10 13:19:21 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,54 +1,26 @@
-# A simple python HTTP server or MQTT broker to accept data(as per your convenience) and save it in a CSV file.
-
-import random
+import socket
 import time
 
-from paho.mqtt import client as mqtt_client
+HEADERSIZE = 10
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1243))
+s.listen(5)
 
-broker = 'broker.emqx.io'
-port = 1883
-topic = "python/mqtt"
+while True:
+   
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established.")
 
-# generate client ID with pub prefix randomly
-client_id = f'python-mqtt-{random.randint(0, 1000)}'
-# username = 'emqx'
-# password = 'public'
+    msg = f"{len(msg):<{HEADERSIZE}}"+msg
 
-def connect_mqtt():
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
+    clientsocket.send(bytes(msg,"utf-8"))
 
-    client = mqtt_client.Client(client_id)
-    client.username_pw_set(username, password)
-    client.on_connect = on_connect
-    client.connect(broker, port)
-    return client
-
-
-def publish(client):
-    msg_count = 0
     while True:
-        time.sleep(1)
-        msg = f"messages: {msg_count}"
-        result = client.publish(topic, msg)
-        # result: [0, 1]
-        status = result[0]
-        if status == 0:
-            print(f"Send `{msg}` to topic `{topic}`")
-        else:
-            print(f"Failed to send message to topic {topic}")
-        msg_count += 1
+        time.sleep(3)
+        msg = f"The time is {time.time()}"
+        msg = f"{len(msg):<{HEADERSIZE}}"+msg
 
+        print(msg)
 
-def run():
-    client = connect_mqtt()
-    client.loop_start()
-    publish(client)
-
-
-if __name__ == '__main__':
-    run()
+        clientsocket.send(bytes(msg,"utf-8"))
